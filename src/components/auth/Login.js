507235b689ccad36155c70bcbaf62969041886bd
JnDiego <diego_suarez@outlook.com>
2020-10-21 20:24:39 -0500

    Se implementa maquetación en el componente NewAccount

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-  const onChangeEmail = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
 
+  const { email, password } = user;
+
+  const onChangeField = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
   }
 
-  const onChangePassword = () => {
+  const onSubmit = e => {
+    e.preventDefault();
 
+    // Validate empty fields
+
+    // Send to action
+    
   }
 
   return (
@@ -16,20 +32,24 @@ const Login = () => {
         <div className="contenedor-form sombra-dark">
           <h1>Sign In</h1>
 
-          <form>
+          <form onSubmit={onSubmit}>
             <div className="campo-form">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" placeholder="Email" onChange={onChangeEmail} />
+              <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={onChangeField} />
             </div>
             <div className="campo-form">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" placeholder="Password" onChange={onChangePassword} />
+              <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={onChangeField} />
             </div>
 
             <div className="campo-form">
               <input type="submit" className="btn btn-primario btn-block" value="Sign In"/>
             </div>
           </form>
+
+          <Link to={'/new-account'} className="enlace-cuenta">
+            Sign Up
+          </Link>
         </div>
       </div>
     </div>
--- src/components/auth/NewAccount.js
+++ src/components/auth/NewAccount.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const NewAccount = () => {
+  
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirm: '',
+  })
+
+  const { username, email, password, confirm } = user;
+
+  const onChangeField = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    // Validate empty fields
+
+    // Validate password with minimum 6 character
+
+    // Validate password and confirm password
+
+    // Send to action
+  }
+
   return (
     <div>
-      <h1>From New Account</h1>
+      <div className="form-usuario">
+        <div className="contenedor-form sombra-dark">
+          <h1>Create your account</h1>
+
+          <form onSubmit={onSubmit}>
+            <div className="campo-form">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" placeholder="Username" value={username} onChange={onChangeField} />
+            </div>
+
+            <div className="campo-form">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={onChangeField} />
+            </div>
+
+            <div className="campo-form">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={onChangeField} />
+            </div>
+
+            <div className="campo-form">
+              <label htmlFor="confirm">Confirm Password</label>
+              <input type="password" id="confirm" name="confirm" placeholder="Confirm Password" value={confirm} onChange={onChangeField} />
+            </div>
+
+            <div className="campo-form">
+              <input type="submit" className="btn btn-primario btn-block" value="Sign Up"/>
+            </div>
+          </form>
+
+          <Link to={'/'} className="enlace-cuenta">
+            Return to sign in
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
