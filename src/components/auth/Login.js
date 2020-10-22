fdf3715eada9142d4eefabe13a4aa2b8b6ea7dc8
JnDiego <diego_suarez@outlook.com>
2020-10-21 20:02:41 -0500

    Se implementa maquetación en el componente Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
 const Login = () => {
+
+  const onChangeEmail = () => {
+
+  }
+
+  const onChangePassword = () => {
+
+  }
+
   return (
     <div>
-      <h1>From Login</h1>
+      <div className="form-usuario">
+        <div className="contenedor-form sombra-dark">
+          <h1>Sign In</h1>
+
+          <form>
+            <div className="campo-form">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder="Email" onChange={onChangeEmail} />
+            </div>
+            <div className="campo-form">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" placeholder="Password" onChange={onChangePassword} />
+            </div>
+
+            <div className="campo-form">
+              <input type="submit" className="btn btn-primario btn-block" value="Sign In"/>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
